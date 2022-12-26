68bcfb354ceb2d06cefb69950d08930188bfae12
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2022-12-26 14:09:05 +0800

    update opencv and vcpkg version, update openblas build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,12 @@
+# Download and install OpenBLAS
+wget https://github.com/xianyi/OpenBLAS/releases/download/v0.3.21/OpenBLAS-0.3.21.tar.gz -O OpenBLAS.tgz
+tar xf OpenBLAS.tgz
+cd OpenBLAS-0.3.21
+make
+make install PREFIX=../OpenBLAS
+cd ..
+export OpenBLAS_HOME="$PWD/OpenBLAS"
+
 # install required 3rd party libraries
 echo "Install vcpkg libraries"
 ./vcpkg/bootstrap-vcpkg.sh
@@ -10,11 +19,21 @@ cmake -Bbuild -H. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="../dist" \
       -DCMAKE_TOOLCHAIN_FILE="../vcpkg/scripts/buildsystems/vcpkg.cmake" \
-      -DWITH_EIGEN=ON -DWITH_TBB=ON -DWITH_OPENGL=ON -DBUILD_TIFF=ON \
-      -DWITH_QT=OFF -DWITH_FFMPEG=OFF \
-      -DBUILD_opencv_apps=OFF -DBUILD_DOCS=OFF -DBUILD_PACKAGE=OFF \
-      -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_JAVA=OFF \
-      -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=OFF \
+      -DWITH_EIGEN=ON \
+      -DWITH_TBB=ON \
+      -DWITH_OPENGL=ON \
+      -DBUILD_TIFF=ON \
+      -DWITH_QT=OFF \
+      -DWITH_OPENEXR=OFF \
+      -DWITH_FFMPEG=OFF \
+      -DBUILD_opencv_apps=OFF \
+      -DBUILD_DOCS=OFF \
+      -DBUILD_PACKAGE=OFF \
+      -DBUILD_PERF_TESTS=OFF \
+      -DBUILD_TESTS=OFF \
+      -DBUILD_JAVA=OFF \
+      -DBUILD_opencv_python2=OFF \
+      -DBUILD_opencv_python3=OFF \
       -DBUILD_LIST=imgcodecs,imgproc,highgui \
       -DBUILD_opencv_world=ON
 
@@ -25,5 +44,6 @@ make install
 
 # pack
 cd ../../dist
+cp -a ../OpenBLAS/lib/*.so lib/
 export XZ_DEFAULTS="-T 0"
 tar Jcf ../opencv-linux.tar.xz *

034feaa035b6286e09e5ca1bb65696758698e80c
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2023-10-04 14:21:11 +0800

    update linux build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,47 @@
+#!/bin/bash
+
 # install required 3rd party libraries
-echo "Install vcpkg libraries"
-./vcpkg/bootstrap-vcpkg.sh
-./vcpkg/vcpkg install eigen3 tbb --triplet x64-linux --clean-after-build
+
+if [ ! -d "./vcpkg/installed/x64-linux-release/lib" ]
+then
+    echo "::group::Install vcpkg libraries ..."
+    # specify triplet
+    export VCPKG_DEFAULT_HOST_TRIPLET=x64-linux-release
+    export VCPKG_DEFAULT_TRIPLET=x64-linux-release
+    ./vcpkg/bootstrap-vcpkg.sh
+    ./vcpkg/vcpkg install intel-mkl libavif tbb --clean-after-build
+    # workaround: opencv failed to detect release triplet
+    ln -s $PWD/vcpkg/installed/x64-linux-release vcpkg/installed/x64-linux
+    echo "::endgroup::"
+fi
+
+
+# Apply patch to submodules
+if [ ! -f "./opencv/cmake/OpenCVFindAOM.cmake" ]
+then
+    echo "::group::Patch libavif ..."
+    git apply --ignore-space-change --ignore-whitespace opencv_libavif.patch
+    echo "::endgroup::"
+fi
 
 # Build opencv
-cd opencv
+echo "::group::Configure CMake and Build ..."
+DIST_PATH="$PWD/dist"
 cmake -Bbuild \
+      -Wno-dev \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_INSTALL_PREFIX="../dist" \
-      -DCMAKE_TOOLCHAIN_FILE="../vcpkg/scripts/buildsystems/vcpkg.cmake" \
-      -DWITH_EIGEN=ON \
+      -DCMAKE_INSTALL_PREFIX="${DIST_PATH}" \
+      -DCMAKE_TOOLCHAIN_FILE="$PWD/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+      -DOPENCV_EXTRA_MODULES_PATH="$PWD/opencv_contrib/modules" \
       -DWITH_TBB=ON \
+      -DWITH_CUDA=ON \
       -DWITH_OPENGL=ON \
       -DBUILD_TIFF=ON \
       -DBUILD_PNG=ON \
       -DBUILD_JPEG=ON \
       -DBUILD_WEBP=ON \
       -DBUILD_OPENJPEG=ON \
+      -DWITH_AVIF=ON \
       -DWITH_QT=OFF \
       -DWITH_OPENEXR=OFF \
       -DWITH_FFMPEG=OFF \
@@ -30,16 +55,22 @@ cmake -Bbuild \
       -DBUILD_opencv_python3=OFF \
       -DCV_TRACE=OFF \
       -DCMAKE_BUILD_RPATH_USE_ORIGIN=TRUE \
-      -DBUILD_LIST="imgcodecs,imgproc,highgui" \
-      -DBUILD_opencv_world=ON
+      -DBUILD_LIST="imgcodecs,imgproc,highgui,cudaimgproc,cudev" \
+      -DBUILD_opencv_world=ON \
+      opencv
+cmake --build build -j 4 -t install
+echo "::endgroup::"
 
-# Build
-cd build
-make -j4
-make install
+# pack binary
+echo "::group::Pack artifacts ..."
 
-# pack
-cd ../../dist
-cp -aP ../OpenBLAS/lib/*.so* lib/
-export XZ_DEFAULTS="-T 0"
-tar Jcf ../opencv-linux.tar.xz *
+cp -a /usr/local/cuda/lib64/libnppc.so* $DIST_PATH/lib
+cp -a /usr/local/cuda/lib64/libnppial.so* $DIST_PATH/lib
+cp -a /usr/local/cuda/lib64/libnppicc.so* $DIST_PATH/lib
+cp -a /usr/local/cuda/lib64/libnppidei.so* $DIST_PATH/lib
+cp -a /usr/local/cuda/lib64/libnppist.so* $DIST_PATH/lib
+
+TARGET=${1:-'opencv-linux.tar.zst'}
+tar "-I zstd -3 -T4 --long=27" -cf $TARGET \
+    -C $DIST_PATH $(cd $DIST_PATH; echo *)
+echo "::endgroup::"

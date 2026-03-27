da434151685355bc26aa22c8bc70d976efed849d
Jimmy Lee <jimmy.m.lee@gmail.com>
2026-03-27 10:16:39 +0800

    fix: use \$PWD instead of hardcoded path in build-test.ps1
    Claude Code assisted

--- scripts/build-test.ps1
+++ scripts/build-test.ps1
@@ -4,5 +4,5 @@ if (Test-Path build-test) {
 cmake -Bbuild-test -G "Visual Studio 17 2022" -A "x64" -Wno-dev test
 cmake --build build-test --config Release
 
-$env:Path += ";C:\Users\code\build-opencv\dist\x64\vc17\bin"
+$env:Path += ";$PWD\dist\x64\vc17\bin"
 Measure-Command { .\build-test\Release\test test\cards.png }
